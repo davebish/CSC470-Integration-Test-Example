89c90a7bfc4e01a10abe1e23e7b54ec6764b7a8b
Dave Bishop <david.b.bishop@gmail.com>
2020-09-13 14:46:59 -0500

    Create shopper and item classes

---
+++ ShoppingIntegrationExample/ShoppingIntegrationExample/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace ShoppingIntegrationExample
+{
+    public class Item
+    {
+        private string Name;
+        private decimal Price;
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+    }
+}
---
+++ ShoppingIntegrationExample/ShoppingIntegrationExample/Shopper.cs
@@ -0,0 +1,14 @@
+﻿namespace ShoppingIntegrationExample
+{
+    public class Shopper
+    {
+        private int Id;
+        private string Name;
+        private string State;
+
+        public void CheckOut()
+        {
+
+        }
+    }
+}
