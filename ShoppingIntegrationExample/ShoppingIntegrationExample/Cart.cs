7ce6aa7d81f6c76c0b5697f5da255550a4995167
Dave Bishop <david.b.bishop@gmail.com>
2020-09-13 15:13:36 -0500

    Cart stuff

---
+++ ShoppingIntegrationExample/ShoppingIntegrationExample/Cart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingIntegrationExample
+{
+    public class Cart
+    {
+        private Dictionary<string, Item> Items = new Dictionary<string, Item>();
+        private Dictionary<string, int> Quantities = new Dictionary<string, int>();
+
+        public void AddItem(Item item, int quantity)
+        {
+            Items.Add(item.GetName(), item);
+            Quantities.Add(item.GetName(), quantity);
+        }
+
+        public decimal TotalPrice()
+        {
+            decimal total = 0.0M;
+
+            foreach (KeyValuePair<string, Item> entry in Items)
+            {
+                total += entry.Value.GetPrice() + Quantities[entry.Value.GetName()];
+            }
+
+            return total;
+        }
+    }
+}
